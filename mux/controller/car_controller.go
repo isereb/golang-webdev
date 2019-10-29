9024e43b40080730d800c751505f468d5aa44e08
isereb <ruxxay.coder@gmail.com>
2019-10-28 23:03:40 -0400

    Added car controller

---
+++ mux/controller/car_controller.go
@@ -0,0 +1,41 @@
+package controller
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+)
+
+type car interface {
+	Name() string
+	Wheels() uint8
+}
+
+type Mercedes string
+func (m *Mercedes) Name() string {
+	return "Mercedes"
+}
+func (m *Mercedes) Wheels() uint8 {
+	return 2
+}
+
+type Toyota string
+func (m *Toyota) Name() string {
+	return "Toyota"
+}
+func (m *Toyota) Wheels() uint8 {
+	return 4
+}
+
+func getText(car car) string {
+	return fmt.Sprintf("My favourite car is %s. It has %d wheels.", car.Name(), string(car.Wheels()))
+}
+
+func (car *Toyota) ServeHTTP(res http.ResponseWriter, req *http.Request) {
+	_, _ = io.WriteString(res, getText(car))
+}
+
+func (car *Mercedes) ServeHTTP(res http.ResponseWriter, req *http.Request) {
+	_, _ = io.WriteString(res, getText(car))
+}
+
