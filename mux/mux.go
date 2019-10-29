4cb851358de900c2a9412f88acab2ce6a21f1455
isereb <ruxxay.coder@gmail.com>
2019-10-28 23:36:43 -0400

    Added basic mux implementation

--- mux/mux.go
+++ mux/mux.go
@@ -1,5 +1,36 @@
 package main
 
+import (
+	"github.com/isereb/golang-webdev/mux/controller"
+	"log"
+	"net/http"
+)
+
 func main() {
 
+	var mercedes controller.Mercedes
+	var toyota controller.Toyota
+
+	mercedesMux := http.NewServeMux()
+	mercedesMux.Handle("/car", mercedes)
+
+	toyotaMux := http.NewServeMux()
+	toyotaMux.Handle("/car", toyota)
+
+	go serveMercedes(mercedes)
+	serveToyota(toyota)
+}
+
+func serveMercedes(mercedes controller.Mercedes) {
+	err := http.ListenAndServe(":11962", mercedes)
+	if err != nil {
+		log.Println("Could not start the mercedes http server: ", err)
+	}
+}
+
+func serveToyota(toyota controller.Toyota) {
+	err := http.ListenAndServe(":11937", toyota)
+	if err != nil {
+		log.Println("Could not start the toyota  http server: ", err)
+	}
 }
