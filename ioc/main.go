73b68c462036f1844c4717f9eba583390890f77f
isereb <ruxxay.coder@gmail.com>
2019-10-31 20:28:00 -0400

    Added simple gorilla mux example

---
+++ ioc/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"./rest"
+	"./services"
+	"flag"
+	"fmt"
+	"github.com/gorilla/mux"
+	"github.com/isereb/golang-webdev/logging/util"
+	"log"
+	"net/http"
+	"os"
+	"time"
+)
+
+func main() {
+
+	port := flag.Int("port", 8081, "-port=8081")
+	flag.Parse()
+
+	util.Info("Starting a program on port", *port)
+
+	svc := make(map[string]services.BaseService, 1)
+	svc["make_service"] = services.BaseService{
+		Name: "make_service",
+	}
+
+	handler := rest.BaseHandler{
+		Services:   &svc,
+		HandlerLog: log.New(os.Stdout, "HANDLER\t", log.LUTC),
+	}
+
+	r := mux.NewRouter()
+	r.HandleFunc("/makes", handler.GetAllMakes).Methods("GET")
+	r.HandleFunc("/make/{make}", handler.GetMake).Methods("GET")
+	//r.HandleFunc("/models", rest.MakesHandler)
+
+	srv := &http.Server{
+		Handler:      r,
+		Addr:         "127.0.0.1:" + fmt.Sprint(*port),
+		WriteTimeout: 15 * time.Second,
+		ReadTimeout:  15 * time.Second,
+	}
+
+	log.Fatal(srv.ListenAndServe())
+}
